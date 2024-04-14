704a2a1a21c01fff1808013ea717ceecfb56d859
CissaJB <102878224+CissaJB@users.noreply.github.com>
2024-04-14 19:53:14 -0300

    Update index.js

--- hooks/functions/index.js
+++ hooks/functions/index.js
@@ -3,3 +3,4 @@ export * from './logMe.js';
 export {checkExistence as checkPruductsExistence} from './products/checkExistence.js';
 export {checkExistence as checkCategoriesExistence} from './categories/checkExistence.js';
 export {checkExistence as checkUsersExistence} from './users/checkExistence.js';
+export * from './users/isAdmin.js';
